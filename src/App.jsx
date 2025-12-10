8de44cc8f92f9bfdf89361d56e8830c77737c80e
anuj paul <paulanuj27@gmail.com>
2025-12-10 07:14:14 +0600

    1.Simple create and coneect server to client site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
   
 function App() {
+  const [users, setUser] = useState([])
 
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res=> res.json())
+    .then(data=> {
+      setUser(data);
+    })
+  },[])
 
   return (
     <>
-
+      <h2>User Management System </h2>
+      {
+        users.map(user=> <p>{user.id}, {user.name}</p> )
+      }
     </>
   )
 }
